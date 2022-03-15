ca366e66d96b5f546d1093c240f72951c1846fa8
expressgy <x71291@outlook.com>
2022-03-15 18:39:04 +0800

    测试路由热更新

--- routes/index.jsx
+++ routes/index.jsx
@@ -1,9 +1,14 @@
-import Index from "../Components/Index/index";
+import Index from "../Components/Index";
+import Test from "../Components/Test";
 
 
 export default [
 	{
 		path:'/',
 		element:<Index/>
+	},
+	{
+		path: '/test',
+		element: <Test/>
 	}
 ]
