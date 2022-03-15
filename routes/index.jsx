e2a02e7d5a45fdd001e1eca274b6e553f47ea8b0
expressgy <x71291@outlook.com>
2022-03-15 18:29:34 +0800

    初始化项目和路由

---
+++ routes/index.jsx
@@ -0,0 +1,9 @@
+import Index from "../Components/Index/index";
+
+
+export default [
+	{
+		path:'/',
+		element:<Index/>
+	}
+]
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
+import React from "react";
+import ReactDom from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+//	以上为核心库
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+import App from "./App";
+import './index.css'
+
+
+ReactDom.render(
+	<React.StrictMode>
+		<BrowserRouter>
+			<App/>
+		</BrowserRouter>
+	</React.StrictMode>,
+	document.getElementById('root')
 )
